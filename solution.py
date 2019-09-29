1b27279f3f73125f349eb2ed24a600606f93d773
Andrey Ustyuzhanin <andrey.u@gmail.com>
2019-09-29 23:12:58 +0300

    Makefile added

--- solution.py
+++ solution.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
+import os
 import sys
 import pandas as pd
 import numpy as np
+
 
 def main():
     # print command line arguments
     input_dir, output_dir = sys.argv[1:]
     predicted_result = []
-    df = np.loadtxt(input_dir + '/data.data')
+    df = np.loadtxt(os.path.join(input_dir, 'data.data'))
     df = pd.DataFrame(df, columns=['column 1', 'column 2', 'column 3'])
 
     df['result'] = df['column 1']
 
-    np.savetxt(output_dir + 'data.solution', np.array(df['result']))
+    np.savetxt(os.path.join(output_dir, 'data.solution'), np.array(df['result']), fmt='%.3f')
     return 0
 
 if __name__ == "__main__":
